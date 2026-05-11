6c7ea6832ea829cb52fc71a73b8d39e3b5586fc8
Japrulll <18223067@std.stei.itb.ac.id>
2026-05-11 23:07:10 +0700

    [feat] Login Box: Background and Header

---
+++ src/components/loginPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function loginPage() {
+  return (
+    <div className='flex bg-gradient min-h-screen select-none items-center justify-center'>
+        <div className='flex flex-col gap-12.5'>
+            <img src='/VotelyNew_White.png' className='h-[53px]'></img>
+            <div className='bg-gray-50/70 border border-white p-8 rounded-lg items-center gap-10'>
+                <div className='flex flex-col items-center text-center'>
+                    <img src='/userlogo.png' className='w-20 h-20'>
+                    </img>
+                    <div className='font-black font-google text-3xl'>
+                        Login
+                    </div>
+                </div>
+
+                <div className='bg-gray'>
+                    a
+                </div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default loginPage
